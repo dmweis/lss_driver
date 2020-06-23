7f0a860748ec5eb40fae788cb3e9bb20a4bf2759
David Michael Weis <dweis7@gmail.com>
2020-06-24 00:04:16 +0100

    Add color changeing to make sure moves finish

--- examples/move_home.rs
+++ examples/move_home.rs
@@ -1,7 +1,5 @@
 use iron_lss;
 use clap::Clap;
-use async_std::task::sleep;
-use std::time::Duration;
 
 #[derive(Clap)]
 #[clap()]
@@ -17,9 +15,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args: Args = Args::parse();
     let mut driver = iron_lss::LSSDriver::new(&args.port).unwrap();
     driver.move_to_position(5, 0.0).await?;
-    // Why does it exit too early if this is here? 
-    // Send doesn't assure that data is actually sent
-    // TODO: figure out how to assure that data is sent
-    sleep(Duration::from_secs_f32(0.3)).await;
+    driver.set_color(5, iron_lss::LedColor::Magenta).await?;
     Ok(())
 }
