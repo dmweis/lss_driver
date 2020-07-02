8f4fd2ed2b7546a2ce44aaccb244be84906e7bee
David Michael Weis <dweis7@gmail.com>
2020-07-02 23:47:30 +0100

    Use broadcast ID in examples

--- examples/move_home.rs
+++ examples/move_home.rs
@@ -12,7 +12,7 @@ struct Args {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Args = Args::parse();
     let mut driver = lss_driver::LSSDriver::new(&args.port).unwrap();
-    driver.move_to_position(5, 0.0).await?;
+    driver.move_to_position(lss_driver::BROADCAST_ID, 0.0).await?;
     driver.set_color(5, lss_driver::LedColor::Magenta).await?;
     Ok(())
 }
--- examples/move_to.rs
+++ examples/move_to.rs
@@ -14,7 +14,7 @@ struct Args {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Args = Args::parse();
     let mut driver = lss_driver::LSSDriver::new(&args.port).unwrap();
-    driver.move_to_position(5, args.position).await?;
-    driver.set_color(5, lss_driver::LedColor::Magenta).await?;
+    driver.move_to_position(lss_driver::BROADCAST_ID, args.position).await?;
+    driver.set_color(lss_driver::BROADCAST_ID, lss_driver::LedColor::Magenta).await?;
     Ok(())
 }
