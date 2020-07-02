a32aa37143d2912f584cd0f4006aa1619a398777
David Michael Weis <dweis7@gmail.com>
2020-07-02 23:46:38 +0100

    Add rename example

---
+++ examples/rename.rs
@@ -0,0 +1,32 @@
+use async_std::task::sleep;
+use clap::Clap;
+use lss_driver;
+use std::time::Duration;
+
+#[derive(Clap)]
+#[clap()]
+struct Args {
+    #[clap(about = "Serial port to use")]
+    port: String,
+    #[clap(about = "ID you want to give to the connected servo", long="id")]
+    id: u8
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Args = Args::parse();
+    let mut driver = lss_driver::LSSDriver::new(&args.port).unwrap();
+    driver.set_color(lss_driver::BROADCAST_ID, lss_driver::LedColor::Red).await?;
+    driver.set_id(lss_driver::BROADCAST_ID, args.id).await?;
+    driver.reset(lss_driver::BROADCAST_ID).await?;
+    // After reset servo becomes unresponsive for a bit
+    sleep(Duration::from_secs(2)).await;
+    let new_id = driver.query_id(lss_driver::BROADCAST_ID).await?;
+    if new_id == args.id {
+        println!("ID set successfully");
+        driver.set_color(lss_driver::BROADCAST_ID, lss_driver::LedColor::Green).await?;
+    } else {
+        eprintln!("ID setting failed!");
+    }
+    Ok(())
+}
